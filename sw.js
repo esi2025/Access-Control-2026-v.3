431e5e05be2b8c42273422ca6d6674db40836ce0
esi2025 <khodam2005@gmail.com>
2026-05-24 09:43:25 +0330

    build: migrate to Tailwind CSS v4 and local assets
    - Migrate from CDN to local Tailwind CSS v4 setup
    - Bundle Vazirmatn font via @fontsource
    - Use local icon file instead of external CDN URL
    - Update Service Worker to cache local assets

--- sw.js
+++ sw.js
@@ -4,9 +4,8 @@ const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100;400;700&display=swap',
-  'https://cdn-icons-png.flaticon.com/512/2912/2912761.png'
+  './icon.png',
+  './index.css'
 ];
 
 // مرحله نصب: کش کردن فایل‌های ضروری
